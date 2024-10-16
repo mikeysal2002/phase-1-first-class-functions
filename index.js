f8894585dacda491d50d25e92d2877086fa059f6
Michael <mikesalgd@gmail.com>
2024-10-16 11:19:00 -0500

    completed lab

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(callback){
+    callback();
+}
+
+function returnsANamedFunction(){
+    function named(){
+        console.log("this if from named function");
+    }
+    return named;
+}
+function returnsAnAnonymousFunction(){
+    return function(){
+        console.log("This is from anonymous function");
+    }
+}
